96eaac92be1966173cc77c96cc688bfecc96ab93
magabysg <gabysthrd@gmail.com>
2026-04-15 14:38:35 -0300

    feat(Ex05): contador de pares

---
+++ src/Exx5.java
@@ -0,0 +1,14 @@
+public class Exx5 {
+
+    void main(){
+
+        int i = 0;
+        while (i <= 50){
+            if (i%2 <= 50) {
+                IO.println(i++);
+               i++;
+
+            }
+        }
+    }
+}
